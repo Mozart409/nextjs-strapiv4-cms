90143ca8d0f364841d5edc49623f1162af70765a
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-12-11 13:22:49 +0100

    Add GraphQL plugin to backend configuration

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,4 +2,11 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  graphql: {
+    enabled: true,
+    config: {
+      // set this to true if you want to enable the playground in production
+      playgroundAlways: false,
+    },
+  },
 });
