93b999fe8ec1e0f1f27dfb72a1c5bc78b9dcaacd
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-12-13 14:49:42 +0100

    Add rest-cache plugin and configure Redis

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -9,4 +9,42 @@ module.exports = ({ env }) => ({
       playgroundAlways: false,
     },
   },
+  redis: {
+    config: {
+      connections: {
+        default: {
+          connection: {
+            host: "127.0.0.1",
+            port: 6379,
+            db: 0,
+          },
+          settings: {
+            debug: true,
+          },
+        },
+      },
+    },
+  },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "redis",
+        options: {
+          max: 32767,
+          connection: "default",
+        },
+      },
+      strategy: {
+        // if you are using keyPrefix for your Redis, please add <keysPrefix>
+        keysPrefix: "strapi",
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::category.category",
+          "api::article.article",
+          "api::global.global",
+          "api::page.page",
+        ],
+      },
+    },
+  },
 });
