204abf302f745323d0ba67e994845ec311c3a651
Amadeus <38767929+Mozart409@users.noreply.github.com>
2024-12-31 13:07:22 +0100

    fix: correct display names in JSON configuration files

--- apps/backend/src/api/page/middlewares/page-populate-middleware.js
+++ apps/backend/src/api/page/middlewares/page-populate-middleware.js
@@ -5,86 +5,89 @@
  */
 
 const populate = {
-  contentSections: {
-    populate: {
-      picture: {
-        fields: ["url", "alternativeText", "caption", "width", "height"],
-      },
-      buttons: {
-        populate: true,
-      },
-      feature: {
-        populate: {
-          fields: ["title", "description", "showLink", "newTab", "url", "text"],
-          media: {
-            fields: ["url", "alternativeText", "caption", "width", "height"],
-          },
-        },
-      },
-      testimonials: {
-        populate: {
-          picture: {
-            fields: ["url", "alternativeText", "caption", "width", "height"],
-          },
-        },
-      },
-      plans: {
-        populate: ["product_features"],
-      },
-      submitButton: {
-        populate: true,
-      },
-      list: {
-        populate: true,
-      },
-      listElement: {
-        populate: true,
-      },
-      seminarCard: {
-        populate: {
-          image: {
-            fields: ["url", "alternativeText", "caption", "width", "height"],
-          },
-        },
-      },
-      richText: {
-        populate: true,
-      },
-      features: {
-        populate: {
-          media: true,
-          link: true,
-        },
-      },
-      featuresGroup: {
-        populate: {
-          features: {
-            populate: {
-              media: true,
-              link: true,
-            },
-          },
-        },
-      },
-    },
-  },
-  seo: {
-    fields: ["metaTitle", "metaDescription"],
-    populate: { shareImage: true },
-  },
+	contentSections: {
+		populate: {
+			picture: {
+				fields: ["url", "alternativeText", "caption", "width", "height"],
+			},
+			buttons: {
+				populate: true,
+			},
+			image: {
+				fields: ["url", "alternativeText", "caption", "width", "height"],
+			},
+			feature: {
+				populate: {
+					fields: ["title", "description", "showLink", "newTab", "url", "text"],
+					media: {
+						fields: ["url", "alternativeText", "caption", "width", "height"],
+					},
+				},
+			},
+			testimonials: {
+				populate: {
+					picture: {
+						fields: ["url", "alternativeText", "caption", "width", "height"],
+					},
+				},
+			},
+			plans: {
+				populate: ["product_features"],
+			},
+			submitButton: {
+				populate: true,
+			},
+			list: {
+				populate: true,
+			},
+			listElement: {
+				populate: true,
+			},
+			seminarCard: {
+				populate: {
+					image: {
+						fields: ["url", "alternativeText", "caption", "width", "height"],
+					},
+				},
+			},
+			richText: {
+				populate: true,
+			},
+			features: {
+				populate: {
+					media: true,
+					link: true,
+				},
+			},
+			featuresGroup: {
+				populate: {
+					features: {
+						populate: {
+							media: true,
+							link: true,
+						},
+					},
+				},
+			},
+		},
+	},
+	seo: {
+		fields: ["metaTitle", "metaDescription"],
+		populate: { shareImage: true },
+	},
 };
 
 module.exports = (config, { strapi }) => {
-  // Add your own logic here.
-  return async (ctx, next) => {
-    ctx.query = {
-      populate,
-      filters: { slug: ctx.query.filters.slug },
-      locale: ctx.query.locale,
-    };
+	// Add your own logic here.
+	return async (ctx, next) => {
+		ctx.query = {
+			populate,
+			filters: { slug: ctx.query.filters.slug },
+			locale: ctx.query.locale,
+		};
 
-    //  console.log("page-populate-middleware.js: ctx.query = ", ctx.query);
+		//  console.log("page-populate-middleware.js: ctx.query = ", ctx.query);
 
-    await next();
-  };
+		await next();
+	};
 };
