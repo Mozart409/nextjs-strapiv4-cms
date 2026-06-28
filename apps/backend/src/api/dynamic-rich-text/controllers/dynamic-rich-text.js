6af95baa1d85e777ff0c18dbdb3acf352de8a19e
Amadeus Mader <amadeus@mozart409.com>
2026-06-28 10:07:11 +0200

    chore(biome): biome format

--- apps/backend/src/api/dynamic-rich-text/controllers/dynamic-rich-text.js
+++ apps/backend/src/api/dynamic-rich-text/controllers/dynamic-rich-text.js
@@ -1,9 +1,9 @@
-"use strict";
-
 /**
  * dynamic-rich-text controller
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::dynamic-rich-text.dynamic-rich-text");
+module.exports = createCoreController(
+  "api::dynamic-rich-text.dynamic-rich-text",
+);
--- apps/backend/src/api/lead-form-submission/controllers/lead-form-submission.js
+++ apps/backend/src/api/lead-form-submission/controllers/lead-form-submission.js
@@ -1,9 +1,9 @@
-"use strict";
-
 /**
  *  lead-form-submission controller
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::lead-form-submission.lead-form-submission");
+module.exports = createCoreController(
+  "api::lead-form-submission.lead-form-submission",
+);
